549dc14bc011ce665cb729e3490c1f1bb41e84f3
kivanc <erturkkivanc@gmail.com>
2023-01-20 16:41:32 +0300

    CardRepo

---
+++ Mango.Services.ShoppingCartAPI/Repository/CartRepository.cs
@@ -0,0 +1,36 @@
+﻿using Mango.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Mango.Services.ShoppingCartAPI.Repository;
+
+public class CartRepository : ICartRepository
+{
+    public async Task<CartDto> GetCartByUserId(string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<CartDto> CreateUpdateCart(CartDto cartDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> RemoveFromCart(int cartDetailsId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> ApplyCoupon(string couponCode, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> RemoveCoupon(string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> ClearCart(string userId)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Mango.Services.ShoppingCartAPI/Repository/ICartRepository.cs
@@ -0,0 +1,13 @@
+﻿using Mango.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Mango.Services.ShoppingCartAPI.Repository;
+
+public interface ICartRepository
+{
+    Task<CartDto> GetCartByUserId(string userId);
+    Task<CartDto> CreateUpdateCart(CartDto cartDto);
+    Task<bool> RemoveFromCart(int cartDetailsId);
+    Task<bool> ApplyCoupon(string couponCode, string userId);
+    Task<bool> RemoveCoupon(string userId);
+    Task<bool> ClearCart(string userId);
+}
