2dd2d46a4dd07bde3a1eefaeccf1f713094a9697
kivanc <erturkkivanc@gmail.com>
2023-01-20 16:31:35 +0300

    DbSet's added

--- Mango.Services.ShoppingCartAPI/DbContext/ApplicationDbContext.cs
+++ Mango.Services.ShoppingCartAPI/DbContext/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Mango.Services.ShoppingCartAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.ShoppingCartAPI.DbContext;
 
@@ -8,5 +9,8 @@ public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
     {
     }
 
-       
+    public DbSet<Product> Products { get; set; }
+    public DbSet<CartHeader> CartHeaders { get; set; }
+    public DbSet<CartDetails> CartDetails { get; set; }
+
 }
